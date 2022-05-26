649c2769ec0f51e150c152e28864be37be46840d
anannanur <anannaanu974@gmail.com>
2022-05-26 18:58:33 +0600

    function for jwt has been created

--- index.js
+++ index.js
@@ -12,6 +12,22 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: "Unauthorized Access" });
+    }
+    const token = authHeader.split(" ")[1];
+    jwt.verify(token, process.env.TOKEN, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ message: "Forbidden access" });
+        }
+        console.log("decoded", decoded);
+        req.decoded = decoded;
+        next();
+    });
+}
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.obg4i.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 const run = async () => {
@@ -32,6 +48,12 @@ const run = async () => {
             res.send(parts);
         });
 
+        app.get('/parts/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const item = await partsCollection.findOne(query)
+            res.send(item)
+        })
        
 
     } finally {
